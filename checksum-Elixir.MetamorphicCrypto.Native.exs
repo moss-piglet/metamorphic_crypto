8de099cdd5f2c216908e6b8adc6fc3f90a6c3e7e
Mark Thayer <markthayer@hey.com>
2026-05-23 22:10:53 -0400

    update precompiled checksums for v0.2.1

---
+++ checksum-Elixir.MetamorphicCrypto.Native.exs
@@ -0,0 +1,7 @@
+%{
+  "libmetamorphic_crypto_nif-v0.2.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:c6d5dea09db9198ece4f1c3325a914729e1a74448a95da3b8c98a11efe830808",
+  "libmetamorphic_crypto_nif-v0.2.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:f5320c033b68a6a51f53ead4ad7dba2fb8a1448d48c0be8c66e61b8303a678aa",
+  "libmetamorphic_crypto_nif-v0.2.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:21957405589583711b66ef34783bbd56a77f0f1467c12492a8cb86689aba5bce",
+  "libmetamorphic_crypto_nif-v0.2.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:b447d1b8f3b5759b3cd7d78971a2953809c981504eef3c4e4ecc9ba41f5be10f",
+  "metamorphic_crypto_nif-v0.2.1-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:bd7f97bc05617e5d1bb2e22e64c89da5af1afac5b5c7656fe7eaa15ef9e9e634",
+}
